c6646f2a0467ee8bb327a4f79b6d7bcee492bfff
Jorge Buitrago <buitrago.jorge@gmail.com>
2024-02-25 17:39:41 +0100

    nuevo

--- app.ts
+++ app.ts
@@ -3,8 +3,12 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  res.send('¡Hola Mundo1!');
+});
+
 app.get('/hola', (req, res) => {
-  res.send('¡Hola Mundo!');
+  res.send('¡Hola Mundo2!');
 });
 
 app.listen(PORT, () => {
